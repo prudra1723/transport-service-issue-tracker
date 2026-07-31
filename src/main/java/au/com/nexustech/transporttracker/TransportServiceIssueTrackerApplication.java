280884d82627aea02acf490a5833db6271640721
Rudra Pandey <22455439@students.latrobe.edu.au>
2026-07-31 14:29:00 +1000

    Initialize spring Boot issue tracker

---
+++ src/main/java/au/com/nexustech/transporttracker/TransportServiceIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package au.com.nexustech.transporttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportServiceIssueTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportServiceIssueTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/au/com/nexustech/transporttracker/TransportServiceIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nexustech.transporttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportServiceIssueTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
